8b8f85060a3dc33f8949b7875685582548d29c05
Goran Lovincic <lovincic@gmail.com>
2022-12-01 22:00:52 +0100

    new app

---
+++ index.js
@@ -0,0 +1,33 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let todayEl = document.getElementById("today")
+let timeEl = document.getElementById("time")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
+
+function doDate()
+{
+    var str = "";
+
+    var days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+
+    var now = new Date();
+
+    str += "Today is: " + days[now.getDay()] + ", " + now.getDate() + " " + months[now.getMonth()] + " " + now.getFullYear() + " " + now.getHours() +":" + now.getMinutes() + ":" + now.getSeconds();
+    document.getElementById("todaysDate").innerHTML = str;
+}
+
+setInterval(doDate, 1000);
+
